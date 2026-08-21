a8999959ebc9375a685308a27b45d968869f0e9d
Madhukar Kumar <150654715+Madhukar-16@users.noreply.github.com>
2026-08-21 23:27:36 +0530

    Time: 3 ms (86.07%), Space: 43.6 MB (9.9%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,52 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()%2!=0) return false;
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+            else{ if(st.size()==0){
+                return false;
+            }
+            if(ch==')'){
+            if(st.peek()=='('){
+                 st.pop();
+            }
+            else{
+                return false;
+            }
+            }
+            if(ch=='}'){
+              if(st.peek()=='{'){
+              st.pop();
+            }
+            else{
+                return false;
+            }
+            }
+            if(ch==']'){
+              if(st.peek()=='['){
+              st.pop();
+            }
+            else{
+                return false;
+            }
+            }
+            
+        }
+
+    }
+    if(st.size()==0)
+    {
+        return true; 
+    }
+    else{
+        return false;
+    }
+    }
+
+
+}
